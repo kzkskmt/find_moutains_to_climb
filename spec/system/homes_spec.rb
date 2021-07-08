21460da79bda214d02c336b8f3fac0e3bd335bdb
kzkskmt <kzk.ottawa@gmail.com>
2021-07-08 14:02:23 +0900

    追加:システムスペックの追加(home)

--- spec/system/homes_spec.rb
+++ spec/system/homes_spec.rb
@@ -5,8 +5,20 @@ RSpec.describe "Homes", type: :system do
     context 'トップページへアクセス' do
       it '正常に表示される' do
         visit root_path
-        expect(page).to have_content '次はどの山にいこう？'
+        expect(page).to have_content '登りたい山がみつかるサービス'
         expect(current_path).to eq root_path
+      end
+    end
+
+    context '地図の表示', js: true do
+      it 'GoogleMapが正常に表示される' do
+        visit root_path
+        within('.gm-style') do
+          expect(page).to have_selector("iframe")
+        end
+      end
+
+      it 'マーカーをクリックするとinWindowが表示される' do
       end
     end
   end
