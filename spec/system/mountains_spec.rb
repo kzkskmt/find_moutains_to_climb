2954d0b33c68e0b871d3374225ce2ddd577dede0
kzkskmt <kzk.ottawa@gmail.com>
2021-07-09 09:34:46 +0900

    追加:システムスペックの追加(mountain))

---
+++ spec/system/mountains_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "Mountains", type: :system do
+  let(:mountain) { create :mountain }
+
+  describe '詳細ページ' do
+    context '詳細ページへアクセス' do
+      it '正常に表示される' do
+        visit mountain_path(mountain.id)
+        expect(page).to have_content mountain.name
+        expect(current_path).to eq mountain_path(mountain.id)
+      end
+    end
+
+    context '天気の表示', js: true do
+      it 'OpenWeathermapが正常に表示される' do
+        visit mountain_path(mountain.id)
+        within('.weather-section') do
+          expect(page).to have_selector(".weather-report")
+        end
+      end
+    end
+  end
+end
