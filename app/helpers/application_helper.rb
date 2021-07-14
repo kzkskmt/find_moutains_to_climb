d852d60e519c680cd9ee3821937ed26f436c2302
kzkskmt <kzk.ottawa@gmail.com>
2021-07-14 17:14:48 +0900

    追加・削除:courseの詳細ページにgooglemap表示、検索機能の実装、ルート検索機能の削除

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,6 @@ module ApplicationHelper
     min_i = min.to_i
     hours = min_i / 60
     mins = min_i - hours * 60
-    sprintf("%d時間%02d分", hours, mins)
+    mins == 0 ? sprintf("%d時間", hours) : sprintf("%d時間%02d分", hours, mins)
   end
 end
