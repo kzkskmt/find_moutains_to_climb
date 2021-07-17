8e9c13b70cd7513e23f06c7d941daa7c02af88ba
kzkskmt <kzk.ottawa@gmail.com>
2021-07-17 18:54:15 +0900

    追加:mountainのシステムスペック追加

--- spec/factories/equipment.rb
+++ spec/factories/equipment.rb
@@ -1,9 +1,27 @@
 FactoryBot.define do
   factory :equipment do
-    title { "MyString" }
-    body { "MyText" }
-    lower_limit_temp { 1 }
-    max_elevation { 1 }
-    image { "MyString" }
+    sequence(:title, "equipment_1")
+    sequence(:body, "body_1")
   end
+
+  trait :temp_15 do
+    lower_limit_temp { 15 }
+  end
+
+  trait :temp_25 do
+    lower_limit_temp { 25 }
+  end
+
+  trait :elevation_1500 do
+    max_elevation { 1500 }
+  end
+
+  trait :elevation_2500 do
+    max_elevation { 2500 }
+  end
+
+  trait :elevation_3800 do
+    max_elevation { 3800 }
+  end
+
 end
