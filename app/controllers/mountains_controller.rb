58f7d4d3e7353f89b3f5434b4d1e55db26d5cb47
kzkskmt <kzk.ottawa@gmail.com>
2021-08-11 18:15:45 +0900

    追加:山の詳細ページにTwitterで最近投稿された山の写真を表示

--- app/controllers/mountains_controller.rb
+++ app/controllers/mountains_controller.rb
@@ -15,6 +15,7 @@ class MountainsController < ApplicationController
     # ① 上限標高(max_elevation)が低い服装パターン順に並べ替える。
     # ② さらに、下限気温(lower_limit_temp)が高い服装パターン順に並べ替えて、先頭２つを取得する（季節が「春秋」か「夏」の２パターンのため）。
     @outfits = Outfit.where('max_elevation > ?', @mountain.elevation).order(max_elevation: :asc, lower_limit_temp: :desc).first(2)
+    @tweet_img_urls = @mountain.search_tweets
   end
 
   def edit
