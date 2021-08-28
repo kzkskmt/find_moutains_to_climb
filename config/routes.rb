9ce99159d2af83f299183b85de6d1a5c2ff73f27
kzkskmt <kzk.ottawa@gmail.com>
2021-08-29 02:53:37 +0900

    修正:mountainのimageploaderの設定変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :mountains, only: %i[index show] do
     # resources :courses, only: %i[index show]
   end
-  resources :outfits, only: %i[index show edit update]
+  resources :outfits, only: %i[index show]
   root 'homes#top'
   get 'terms_of_use', to: 'homes#terms_of_use'
   get 'privacy_policy', to: 'homes#privacy_policy'
