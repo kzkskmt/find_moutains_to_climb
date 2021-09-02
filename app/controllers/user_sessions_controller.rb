112f5a75f3d4153bba393f9a28755d2143a97236
kzkskmt <kzk.ottawa@gmail.com>
2021-09-02 13:11:31 +0900

    追加:フラッシュメッセージの追加

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,8 +6,9 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_back_or_to root_path
+      redirect_back_or_to root_path, success: 'ログインしました'
     else
+      flash.now[:danger] = 'ログインできませんでした'
       render :new
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,10 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save!
-      redirect_to root_url
+    if @user.save
+      redirect_to root_url, success: 'ユーザー登録が完了しました'
     else
+      flash.now[:danger] = 'ユーザー登録できませんでした'
       render :new
     end
   end
