8125a30df1a58af3d7090d30b2c2da263de07b79
kzkskmt <kzk.ottawa@gmail.com>
2021-09-06 22:07:12 +0900

    追加:ヘッダーにリンクを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      auto_login(@user)
       redirect_to root_url, success: t('.success')
     else
       flash.now[:danger] = t '.fail'
