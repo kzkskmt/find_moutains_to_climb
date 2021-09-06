86717aa69628987b6cdd41d4843ed0eb53d2479d
kzkskmt <kzk.ottawa@gmail.com>
2021-09-06 13:41:30 +0900

    追加:投稿編集機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,15 +2,30 @@ class PostsController < ApplicationController
 
   def new
     @post = current_user.posts.build
+    @post.mountain_id = params[:mountain_id] if params[:mountain_id]
   end
 
   def create
-    @post = current_user.posts.build(params_post)
+    @post = current_user.posts.build(post_params)
     if @post.save
       redirect_to user_path(current_user), success: t('.success')
     else
       flash.now[:danger] = t '.fail'
       render :new
+    end
+  end
+
+  def edit
+    @post = current_user.posts.find(params[:id])
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+    if @post.update(post_params)
+      redirect_to user_path(@post.user), success: t('.success')
+    else
+      flash[:danger] = t '.fail'
+      render :edit
     end
   end
 
@@ -23,7 +38,7 @@ class PostsController < ApplicationController
 
   private
 
-  def params_post
+  def post_params
     params.require(:post).permit(:title, :body, :mountain_id)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :mountain
+
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :body, presence: true, length: { maximum: 1_000 }
 end
