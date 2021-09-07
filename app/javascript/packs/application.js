d7656919eba9e347c18f9f57f96f5c4a04e3ba57
kzkskmt <kzk.ottawa@gmail.com>
2021-09-07 11:20:14 +0900

    追加:フラッシュメッセージのフェードアウト2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -64,5 +64,5 @@ window.addEventListener('DOMContentLoaded', event => {
 
 // フラッシュメッセージのフェードアウト
 $(function(){
-  setTimeout("$('.alert').fadeOut('slow')", 2000);
+  setTimeout("$('#flash').fadeOut('slow')", 2000);
 });
