ae135c125705f1b968f6fb96e2eac33bcb509297
kzkskmt <kzk.ottawa@gmail.com>
2021-09-09 19:47:22 +0900

    修正:コントローラ修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,7 +6,7 @@ class LikesController < ApplicationController
   end
 
   def destroy
-    like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
+    like = current_user.likes.find_by(post_id: params[:post_id], user_id: current_user.id)
     like.destroy!
     redirect_back(fallback_location: root_path)
   end
