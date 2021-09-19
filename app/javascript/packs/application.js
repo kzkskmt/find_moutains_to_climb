d8fa750d1d0cfce1b48c8634136943112ffac1f5
kzkskmt <kzk.ottawa@gmail.com>
2021-09-19 18:31:02 +0900

    追加:youtube.jsを追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
+require("youtube.js")
 require("google_maps.js")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
