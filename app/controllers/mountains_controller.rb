771bccb60b0fef862125dd99304d311cb1d248d5
kzkskmt <kzk.ottawa@gmail.com>
2021-09-27 15:46:47 +0900

    修正:共通処理をconcernsに集約

--- app/controllers/mountains_controller.rb
+++ app/controllers/mountains_controller.rb
@@ -1,4 +1,5 @@
 class MountainsController < ApplicationController
+  include Common
   skip_before_action :require_login
   before_action :set_q, only: %i[index]
   before_action :set_center_of_jp, only: %i[index]
