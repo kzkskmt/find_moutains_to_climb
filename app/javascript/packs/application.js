64efe1241f74be1f3f7827921c7184c0821278aa
kzkskmt <kzk.ottawa@gmail.com>
2021-10-06 17:45:07 +0900

    削除:使っていないspec関連のファイルを削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -42,15 +42,6 @@ window.addEventListener('DOMContentLoaded', event => {
   // Shrink the navbar when page is scrolled
   document.addEventListener('scroll', navbarShrink);
 
-  // Activate Bootstrap scrollspy on the main nav element
-//   const mainNav = document.body.querySelector('#mainNav');
-//   if (mainNav) {
-//       new bootstrap.ScrollSpy(document.body, {
-//           target: '#mainNav',
-//           offset: 74
-//       });
-//   };
-
   // Collapse responsive navbar when toggler is visible
   const navbarToggler = document.body.querySelector('.navbar-toggler');
   const responsiveNavItems = [].slice.call(
